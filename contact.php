9c19f3502daf0300ba84520dc6c4de228bbe2161
ruchita-oza <70565213+ruchita-oza@users.noreply.github.com>
2021-03-27 23:45:35 +0530

    Added Events- create delete

--- contact.php
+++ contact.php
@@ -3,15 +3,15 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>Contact Us</title>
-        <?php require 'utils/styles.php'; ?><!--css links. file found in utils folder-->
-        <?php require 'utils/scripts.php'; ?><!--js links. file found in utils folder-->
+        <?php require 'utils/styles.php'; ?><!--css -->
+        <?php require 'utils/scripts.php'; ?><!--js -->
     </head>
     <body>
-        <?php require 'utils/header.php'; ?><!--header content. file found in utils folder-->
-        <div class = "content"><!--body content holder-->
-            <div class = "container">
-                <div class = "col-md-12"><!--body content title holder with 12 grid columns-->
-                    <h1>Contact Us</h1><!--body content title-->
+        <?php require 'utils/header.php'; ?><!--header -->
+        <div class = "content"><!--body -->
+            <div class = "container"
+                <div class = "col-md-12"><!-- 12 grid columns-->
+                    <h1>Contact Us</h1><!-- title-->
                     <p>Fell Free to Contact us...!</p>
                 </div>
             </div>
@@ -24,10 +24,9 @@
             
             <div class="container">
                 <div class="col-md-6 contacts">
-                    <h1><span class="glyphicon glyphicon-user"></span> Joe Christain & Anna Peter</h1>
+                    <h1><span class="glyphicon glyphicon-user"></span> Hetvee  & Ruchita </h1>
                     <p>
-                        <span class="glyphicon glyphicon-envelope"></span> Email: joechristain@gmail.com / anapeter@gmail.com<br>
-                        <span class="glyphicon glyphicon-phone"></span> Mobile: 08712345678, 08756345625
+                        <span class="glyphicon glyphicon-envelope"></span> Email: hetvee.sakaria@gmail.com / ruchitaoza9@gmail.com<br>
                     </p>
                 </div>
                 <div class="col-md-6">
@@ -50,7 +49,7 @@
             </div>
 			
             
-        </div><!--body content div-->
-        <?php require 'utils/footer.php'; ?><!--footer content. file found in utils folder-->
+        </div><!--body -->
+        <?php require 'utils/footer.php'; ?><!--footer -->
     </body>
 </html>
